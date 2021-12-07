ba3130f5c212a1851c4b881cef1dc0e8de512cac
Rodrigo Santos de Carvalho <rodrigosc2401@gmail.com>
2021-12-07 11:56:05 -0300

    fix 5.py

--- 5.dip.py
+++ 5.dip.py
@@ -4,25 +4,39 @@ Dependency Inversion Principle
 Dependências devem ser feitas sobre abstrações, não sobre implementações concretas 
 
 """
+from abc import ABC
 
-
-class Player:
-    def __init__(self, name):
-        self.stats = StatsReporter(self)
+class Stats(ABC):
+    def __init__(self, name, hp):
         self.__name = name
-        self.__hp = 100
-        self.__speed = 1
+        self.__hp = hp
 
+    @property
+    def hp(self):
+            return self.__hp
+        
+    @property
+    def name(self):
+        return self.__name
+    
+class Player(Stats):
+    def __init__(self, stats:Stats):
+        self.__name = stats.name
+        self.__hp = stats.hp
+        self.__speed = 1
+        
+    @property
     def hp(self):
         return self.__hp
 
+    @property
     def name(self):
         return self.__name
 
-class StatsReporter:
-    def __init__(self, char: Player):
-        self.char = char
+class StatsReporter(Stats):
+    def __init__(self, stats: Stats):
+        self.stats = stats
 
     def report(self):
-        print(f'Name:{self.char.name()}')
-        print(f'HP:{self.char.hp()}')
+        print(f'Name:{self.stats.name()}')
+        print(f'HP:{self.stats.hp()}')

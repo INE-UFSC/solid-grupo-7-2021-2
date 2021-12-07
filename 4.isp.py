2824657200594660ca4f557ffa9b32d97c97be62
Stephan Krug <stekrug@icloud.com>
2021-12-07 13:49:52 -0300

    fix 4.isp.py

--- 4.isp.py
+++ 4.isp.py
@@ -4,43 +4,35 @@ Crie interfaces que são específicas. Clientes não devem depender de interface
 """
 from abc import ABC
 
-class IJanela(ABC):
+class IJanelaSemMenu(ABC):
     def maximizar(self):
         raise NotImplementedError
-    
+
     def minimizar(self):
         raise NotImplementedError
-    
-    def mostrar_menu(self):
-        raise NotImplementedError
-        
+
     def fechar(self):
         raise NotImplementedError
 
-class JanelaTamanhoFixo(IJanela):
-    def maximizar(self):
-        pass
+class IJanelaTamanhoFixo(ABC):
+    def mostrar_menu(self):
+        raise NotImplementedError
 
-    def minimizar(self):
-        pass
-    
+    def fechar(self):
+        raise NotImplementedError
+
+class JanelaTamanhoFixo(IJanelaTamanhoFixo):
     def mostrar_menu(self):
         pass
     
     def fechar(self):
         pass
-
-class JanelaSemMenu(IJanela):
+class JanelaSemMenu(IJanelaSemMenu):
     def maximizar(self):
         pass
 
     def minimizar(self):
         pass
     
-    def mostrar_menu(self):
-        pass
-    
     def fechar(self):
         pass
-
-
